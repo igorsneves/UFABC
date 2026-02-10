d995236621c30c9d850417b93ef3c4c4f6f5714a
igorsneves <140032909+igorsneves@users.noreply.github.com>
2026-02-09 21:49:59 -0300

    Add attendance check to approval condition

--- semana-2-aula1.java
+++ semana-2-aula1.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class PrimeiroprojetoPI {
 
     public static void main(String[] args) {
+      int numFaltas = 0;
       double nota1, nota2, media;
       Scanner coleta = new Scanner(System.in);
       
@@ -12,10 +13,12 @@ public class PrimeiroprojetoPI {
       nota1 = coleta.nextDouble();
       System.out.print("Digite a nota 2: ");
       nota2 = coleta.nextDouble ();
+      System.out.print("Digite o numero de faltas: ");
+      numFaltas = coleta.nextInt ();
       
       media = ( nota1 + nota2 ) / 2;
 
-      if (media > 5) {
+      if ((media > 5) && ( numFaltas <= 12 )){
           System.out.print("Você foi aprovado!");
       }
       else {
