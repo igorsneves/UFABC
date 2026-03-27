090db6186699f13317037106eaac22ae64bd1c73
igorsneves <140032909+igorsneves@users.noreply.github.com>
2026-03-27 20:06:42 -0300

    Update PI.ordenar.linhas.matriz.java

--- PI.ordenar.linhas.matriz.java
+++ PI.ordenar.linhas.matriz.java
@@ -1,25 +1,29 @@
-package primeiroprojetopi.processamentodainformacao;
 
 import java.util.Scanner;
 
 public class testes {
   public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
+        
+        System.out.print("Digite a quantidade de linhas e colunas:");
 
         int linhas = teclado.nextInt();
         int colunas = teclado.nextInt();
+        int i, j, k;
 
         int[][] matriz = new int[linhas][colunas];
+        
+        System.out.print("Digite os valores: ");
 
-        for (int i = 0; i < linhas; i++) {
-            for (int j = 0; j < colunas; j++) {
+        for (i = 0; i < linhas; i++) {
+            for (j = 0; j < colunas; j++) {
                 matriz[i][j] = teclado.nextInt();
             }
         }
 
-        for (int i = 0; i < linhas; i++) {
-            for (int j = 0; j < colunas - 1; j++) {
-                for (int k = 0; k < colunas - 1 - j; k++) {
+        for (i = 0; i < linhas; i++) {
+            for (j = 0; j < colunas - 1; j++) {
+                for (k = 0; k < colunas - 1 - j; k++) {
                     if (matriz[i][k] > matriz[i][k + 1]) {
                         int temp = matriz[i][k];
                         matriz[i][k] = matriz[i][k + 1];
@@ -29,8 +33,8 @@ public class testes {
             }
         }
 
-        for (int i = 0; i < linhas; i++) {
-            for (int j = 0; j < colunas; j++) {
+        for (i = 0; i < linhas; i++) {
+            for (j = 0; j < colunas; j++) {
                 System.out.print(matriz[i][j] + " ");
             }
             System.out.println();
